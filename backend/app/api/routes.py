3dbdd6c846a1c64b6467588da903c88a56343b7c
divakar maurya <divakarmaurya@divakars-MacBook-Air.local>
2026-07-18 13:58:12 +0530

    feat(milestone-1): implement GitHub repository cloning

---
+++ backend/app/api/routes.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from app.models.repository import RepositoryRequest
+from app.core.git_clone import clone_repository
+
+router = APIRouter()
+
+@router.post("/clone")
+def clone_repo(request: RepositoryRequest):
+
+    result = clone_repository(request.repo_url)
+
+    return result
---
+++ backend/app/core/git_clone.py
@@ -0,0 +1,43 @@
+from git import Repo
+from git.exc import GitCommandError
+from pathlib import Path
+import shutil
+
+# backend folder
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# backend/repos
+REPO_DIR = BASE_DIR / "repos"
+
+REPO_DIR.mkdir(exist_ok=True)
+
+
+def clone_repository(repo_url: str):
+
+    repo_name = repo_url.rstrip("/").split("/")[-1]
+
+    destination = REPO_DIR / repo_name
+
+    if destination.exists():
+        shutil.rmtree(destination)
+
+    try:
+        Repo.clone_from(
+            repo_url,
+            destination,
+            depth=1,
+            single_branch=True
+        )
+
+        return {
+            "success": True,
+            "repository": repo_name,
+            "path": str(destination)
+        }
+
+    except GitCommandError as e:
+
+        return {
+            "success": False,
+            "error": str(e)
+        }
